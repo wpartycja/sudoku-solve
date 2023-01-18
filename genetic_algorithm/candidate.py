809afd34aab1b1b4a8a7767fc7bcd2a95de603ae
Patrycja Wysocka <w512patrycja@gmail.com>
2023-01-18 15:16:35 +0100

    added func to calculate quality

--- genetic_algorithm/candidate.py
+++ genetic_algorithm/candidate.py
@@ -8,9 +8,9 @@ from global_var import DIM
 class Candidate:
     def __init__(self, table: Optional[np.array] = None) -> None:
         self.table = np.zeros((DIM, DIM), dtype=int) if table is None else table
-        self.quality = None
+        self.quality = 0
 
-# Check if there will beduplicates
+# Check numbers that already exists in row/column/block
 
     def is_number_in_row(self, row: int, number: int) -> bool:
         """
@@ -48,3 +48,68 @@ class Candidate:
                 values.add(self.table[row][column])
 
         return True if number in values else False
+
+# Calculating quality
+
+    def calculate_row_quality(self) -> float:
+        """
+        method calculates quality looking only for rows
+        """
+        row_quality = 0
+        row_values = set()
+
+        for row in self.table:
+            for cell in row:
+                row_values.add(cell)
+
+            row_quality += len(row_values)/DIM/DIM
+            row_values = set()
+
+        return row_quality
+
+    def calculate_column_quality(self) -> float:
+        """
+        method calculates quality looking only for columns
+        """
+        column_quality = 0
+        column_values = set()
+
+        for column in range(DIM):
+            for row in range(DIM):
+                column_values.add(self.table[row][column])
+
+            column_quality += len(column_values)/DIM/DIM
+            column_values = set()
+
+        return column_quality
+
+    def calculate_block_quality(self) -> float:
+        """
+        method calculates quality looking only for blocks
+        """
+        block_quality = 0
+        block_values = set()
+
+        for column in range(0, DIM, 3):
+            for row in range(0, DIM, 3):
+
+                for i in range(row, row + 3):
+                    for j in range(column, column + 3):
+                        block_values.add(self.table[i][j])
+
+                block_quality += len(block_values)/DIM/DIM
+                block_values = set()
+
+        return block_quality
+
+    def update_quality(self) -> None:
+        """
+        if there are no duplicates nowhere quality = 1
+        if there are somewhere quality would be lower
+        """
+
+        row_quality = self.calculate_row_quality()
+        column_quality = self.calculate_column_quality()
+        block_quality = self.calculate_block_quality()
+
+        self.quality = block_quality * column_quality * row_quality
