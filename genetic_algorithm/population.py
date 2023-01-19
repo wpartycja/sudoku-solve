f9ef3d0ad897e5602958bf2beb8fa4cafb78345f
Patrycja Wysocka <w512patrycja@gmail.com>
2023-01-19 18:16:39 +0100

    wroking find legal values

---
+++ genetic_algorithm/population.py
@@ -0,0 +1,52 @@
+from candidate import Candidate
+from global_var import DIM
+
+import random
+import numpy as np
+
+class Population():
+
+    def __init__(self, base_table):
+        self.candidates = []
+        self.legal_values = self.find_legal_values(base_table)
+
+    def find_legal_values(self, base_table):
+
+        legal_table = [[[] for j in range(DIM)] for i in range(DIM)]
+        legal_solutions = Candidate(base_table, legal_table)
+
+        for row in range(DIM):
+            for column in range(DIM):
+                for value in range(10):
+                    # Check for legal value in every cell
+                    if (base_table[row][column] == 0) and not (
+                        legal_solutions.is_number_in_column(column, value)
+                    ) and not (legal_solutions.is_number_in_row(row, value)
+                    ) and not (legal_solutions.is_number_in_block(row, column, value)):
+                        legal_solutions.table[row][column].append(value)
+
+                    # if value is already known (given at the begging)
+                    elif base_table[row][column] != 0:
+                        legal_solutions.table[row][column].append(base_table[row][column])
+                        break
+
+        return legal_solutions.table
+
+    def new_population(self, base_table, cand_nr):
+
+        for _ in range(cand_nr):
+            cand = Candidate(base_table)
+
+            for i in range(DIM):  # new row in candidate
+                while len(set(cand.table[i])) != 9:  # we want a valid row
+                    for j in range(DIM):
+                        if base_table[i][j] != 0:  # if the place is declared in base table
+                            cand.table[i][j] = base_table[i][j]
+                        else:
+                            cand.table[i][j] = random.choice(self.legal_values[i][j])
+                
+            self.candidates.append(cand)
+        
+    def calculate_population_quality(self):
+        for candidate in self.candidates:
+            candidate.update_quality()
